a0ba1aff9a81226b03b381be25a5c6476bc56360
mnonnenm <mnonnenm@uos.de>
2020-03-05 15:03:30 +0100

    updating network trainer und going to full data (1979-2015) - I/O for more than single year extremely slow due to dask out-of-order ?!

--- run_cnn_pgu_pytorch.py
+++ run_cnn_pgu_pytorch.py
@@ -26,82 +26,73 @@ n_channels = len(dataset_list) # = 1 if only loading one of geopotential Z500 an
 
 # tbd: separating train and test datasets / loaders should be avoidable with the start/end arguments of Dataset!
 
-dg_train = Dataset(x.sel(time=slice('2015', '2015')), var_dict, lead_time, normalize=True)
+dg_train = Dataset(x.sel(time=slice('1979', '2015')), var_dict, lead_time, normalize=True)
 train_loader = torch.utils.data.DataLoader(
     dg_train,
     batch_size=batch_size,
     drop_last=True)
 
-dg_test =  Dataset(x.sel(time=slice('2016', '2016')), var_dict, lead_time,
+dg_validation =  Dataset(x.sel(time=slice('2016', '2016')), var_dict, lead_time,
                         mean=dg_train.mean, std=dg_train.std, normalize=True)
-test_loader = torch.utils.data.DataLoader(
-    dg_test,
+validation_loader = torch.utils.data.DataLoader(
+    dg_validation,
     batch_size=batch_size,
     drop_last=False)
 
 ## define model
 
-import torch.nn.functional as F
+from src.train_nn_pytorch import SimpleCNN
 
-class PeriodicConv2D(torch.nn.Conv2d):
-    """ Implementing 2D convolutional layer with mixed zero- and circular padding.
-    Uses circular padding along last axis (W) and zero-padding on second-last axis (H)
-    
-    """
-    def conv2d_forward(self, input, weight):
-        if self.padding_mode == 'circular':
-            expanded_padding_circ = ( (self.padding[0] + 1) // 2, self.padding[0] // 2, 0, 0)
-            expanded_padding_zero = ( 0, 0, (self.padding[1] + 1) //2, self.padding[1] // 2 )
-            return F.conv2d(F.pad(F.pad(input, expanded_padding_circ, mode='circular'), 
-                                  expanded_padding_zero, mode='constant'),
-                            weight, self.bias, self.stride,
-                            (0,0), self.dilation, self.groups)
-        return F.conv2d(input, weight, self.bias, self.stride,
-                        self.padding, self.dilation, self.groups)
-
-class Net(torch.nn.Module):
-    
-    def __init__(self, filters, kernels, channels, activation):
-        super(Net, self).__init__()
-        self.layers, in_ = [], channels
-        self.activation = activation
-        assert not np.any(kernels == 2), 'kernel size 2 not allowed for circular padding'
-        in_channels = [channels] + list(filters[:-1])
-        self.layers = torch.nn.ModuleList([PeriodicConv2D(i, f, k, padding=(k-1, k-1),
-                        padding_mode='circular') for i,f,k in zip(in_channels, filters, kernels)])
-        
-    def forward(self, x):
-        for layer in self.layers[:-1]:
-            x = self.activation(layer(x))
-        x = self.layers[-1](x)
-        return x
-
-net = Net(filters=[64, 64, 64, 64, n_channels], kernels=[5, 5, 5, 5, 5], 
-          channels=n_channels, activation=torch.nn.functional.elu)
+net = SimpleCNN(filters=[64, 64, 64, 64, n_channels], kernels=[5, 5, 5, 5, 5], 
+          channels=n_channels, activation=torch.nn.functional.elu, mode='circular')
 
 ## train model
 
 import torch.optim as optim
-optimizer = optim.Adam(net.parameters(), lr=0.01)
+import torch.nn.functional as F
+from copy import deepcopy
 
-n_epochs = 200
+optimizer = optim.Adam(net.parameters(), lr=0.001)
+
+n_epochs, max_patience = 200, 20
+losses, best_loss, patience = np.zeros(n_epochs), np.inf, max_patience
+best_state_dict = {}
+
 epoch = 0
 while True:
 
-    i = 0
     epoch += 1
+    if epoch > n_epochs:
+        break
+
     # Train for a single epoch.
     for batch in train_loader:
-        print('minibatch #' + str(i))
         optimizer.zero_grad()
         inputs, targets = batch[0].to(device), batch[1].to(device)
         loss = F.mse_loss(net.forward(inputs), targets)
         loss.backward()
-        optimizer.step()        
-        i += 1
+        optimizer.step()
 
-    # tbd: write early stopping from convergence on vakidation data
-    if epoch > n_epochs:
+    # Track convergence on validation set.
+    val_loss = 0
+    with torch.no_grad():
+        nb = 0
+        for batch in validation_loader:
+            inputs, targets = batch[0].to(device), batch[1].to(device)
+            val_loss += F.mse_loss(net.forward(inputs), targets)
+            nb += 1
+    val_loss /= nb
+    print(f'epoch #{epoch} || loss (last batch) {loss} || validation loss {val_loss}')
+
+    if val_loss < best_loss:
+        patience = max_patience
+        best_loss = val_loss
+        best_state_dict = deepcopy(net.state_dict())
+    else:
+        patience -= 1
+
+    if patience == 0:
+        net.load_state_dict(best_state_dict)
         break
         
 torch.save(net.state_dict(), res_dir + 'test_fccnn_3d_pytorch.pt')
