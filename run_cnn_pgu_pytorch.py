9ecd24877805b22bd499afe8f10cbc67a8c723f3
mnonnenm <mnonnenm@uos.de>
2020-03-18 14:45:13 +0100

    updating script to include torchvision FCN ResNet-50

--- run_cnn_pgu_pytorch.py
+++ run_cnn_pgu_pytorch.py
@@ -16,13 +16,14 @@ else:
 datadir = '/gpfs/work/nonnenma/data/forecast_predictability/weatherbench/5_625deg/'
 res_dir = '/gpfs/work/nonnenma/results/forecast_predictability/weatherbench/5_625deg/'
 
-model_fn = '9D_fcUnet_5d_pytorch.pt' # file name for saving/loading prediction model
+use_fields = 'z_t' #'z_t_unilevel_const'
+model_name = 'tvfcnResnet50' # 'tv_fcn_resnet50', 'cnnbn', 'Unetbn'
 
-lead_time = 5*24
+lead_time = 3*24
 batch_size = 32
 
 """
-# geopotential and tempearture each at 11 levels 
+# geopotential and temperature each at 11 levels 
 # regression target is Z500 (level index i=6) and T850 (level index 9, stacked vector index 9+11=20)
 geop = xr.open_mfdataset(f'{datadir}geopotential/*.nc', combine='by_coords')
 temp = xr.open_mfdataset(f'{datadir}temperature/*.nc', combine='by_coords')
@@ -33,7 +34,7 @@ cowu = xr.open_mfdataset(f'{datadir}u_component_of_wind/*.nc', combine='by_coord
 cowv = xr.open_mfdataset(f'{datadir}v_component_of_wind/*.nc', combine='by_coords')
 """
 
-# geopotential and tempearture each at target pressure levels 
+# geopotential and temperature each at target pressure levels 
 z500 = xr.open_mfdataset(f'{datadir}geopotential_500/*.nc', combine='by_coords')
 t850 = xr.open_mfdataset(f'{datadir}temperature_850/*.nc', combine='by_coords')
 
@@ -53,33 +54,36 @@ for var in [cnst.orography, cnst.lsm, cnst.slt, cnst.lat2d, cnst.lon2d]:
                                 name=var.name,indexes=template.indexes)
 cnst = xr.Dataset(data_vars=dataarrays)
 
+if use_fields == 'z_t_unilevel_const':
+    # merging different fields into single dataset (this can take long, and a lot of RAM!)
+    x = xr.merge([z500, t850, tisr, clou, cnst], compat='override', fill_value=0) # fill_value for tisr !
+    x = x.chunk({'time' : np.sum(x.chunks['time']), 
+                 'lat' : x.chunks['lat'], 'lon': x.chunks['lon']})
 
-# merging different fields into single dataset (this can take long, and a lot of RAM!)
-x = xr.merge([z500, t850, tisr, clou, cnst], compat='override', fill_value=0) # fill_value for tisr !
-x = x.chunk({'time' : np.sum(x.chunks['time']), 
-             'lat' : x.chunks['lat'], 'lon': x.chunks['lon']})
+    # dictionary of used variables and their levels for Dataset() objects
+    var_dict = {'z': None,          # target
+                't': None,          # target
+                'tisr' : None,      # extra field
+                'tcc' : None,       # extra field
+                'orography' : None, # constant
+                'lsm' : None,       # constant
+                'slt' : None,       # constant
+                'lat2d' : None,     # constant
+                'lon2d': None}      # constant
+    
+elif use_fields == 'z_t':
 
-# dictionary of used variables and their levels for Dataset() objects
-var_dict = {'z': None,          # target
-            't': None,          # target
-            'tisr' : None,      # extra field
-            'tcc' : None,       # extra field
-            'orography' : None, # constant
-            'lsm' : None,       # constant
-            'slt' : None,       # constant
-            'lat2d' : None,     # constant
-            'lon2d': None}      # constant
-"""
+    # merging different fields into single dataset (this can take long, and a lot of RAM!)
+    x = xr.merge([z500, t850], compat='override', fill_value=0) # fill_value for tisr !
+    x = x.chunk({'time' : np.sum(x.chunks['time']), 
+                 'lat' : x.chunks['lat'], 'lon': x.chunks['lon']})
 
-# merging different fields into single dataset (this can take long, and a lot of RAM!)
-x = xr.merge([z500, t850], compat='override', fill_value=0) # fill_value for tisr !
-x = x.chunk({'time' : np.sum(x.chunks['time']), 
-             'lat' : x.chunks['lat'], 'lon': x.chunks['lon']})
+    # dictionary of used variables and their levels for Dataset() objects
+    var_dict = {'z': None,          # target
+                't': None}          # target
 
-# dictionary of used variables and their levels for Dataset() objects
-var_dict = {'z': None,          # target
-            't': None}          # target
-"""
+else:
+    raise NotImplementedError()
 
 # tbd: separating train and test datasets / loaders should be avoidable with the start/end arguments of Dataset!
 dg_train = Dataset(x.sel(time=slice('1979', '2015')), var_dict, lead_time, normalize=True)
@@ -98,27 +102,76 @@ validation_loader = torch.utils.data.DataLoader(
 n_channels = len(dg_train.data.level.level)
 print('n_channels', n_channels)
 
+model_fn = f'{n_channels}D_fc{model_name}_{lead_time//24}d_pytorch.pt' # file name for saving/loading prediction model
+print('model filename', model_fn)
+
 
 ## define model
 
-from src.train_nn_pytorch import CircUNet
 
-filters =  [ [32], [32], [32], [32]] 
-kernels =  [ [5],  [5], [5], [5] ]
-pooling = 2
+if model_name == 'cnnbn':
+    from src.train_nn_pytorch import SimpleCNN
 
-activation = torch.nn.functional.elu
-mode='circular'
+    filters = [64, 64, 64, 64, 2] # last '2' for Z500, T850
+    kernels = [5, 5, 5, 5, 5]
+    activation = torch.nn.functional.elu
+    mode='circular'
+
+    model = SimpleCNN(filters=filters,
+                      kernels=kernels,
+                      channels=n_channels, 
+                      activation=activation, 
+                      mode=mode)
+
+    def model_forward(input):
+        return model.forward(input)
+
+elif model_name == 'Unetbn':
+    from src.train_nn_pytorch import CircUNet
+
+    filters =  [ [32], [32], [32], [32]] 
+    kernels =  [ [5],  [5], [5], [5] ]
+    pooling = 2
+
+    activation = torch.nn.functional.elu
+    mode='circular'
+
+    model = CircUNet(in_channels=n_channels,
+                     filters=filters,
+                     kernels=kernels,
+                     pooling=pooling,
+                     activation=activation, 
+                     out_channels=2,
+                     mode=mode)
     
-model = CircUNet(in_channels=n_channels,
-                 filters=filters,
-                 kernels=kernels,
-                 pooling=pooling,
-                 activation=activation, 
-                 out_channels=2,
-                 mode=mode)
+    def model_forward(input):
+        return model.forward(input)
+
+elif model_name == 'tvfcnResnet50':
+    import torchvision
+    k = 3
+    
+    model = torchvision.models.segmentation.fcn_resnet50(pretrained=False)
+
+    # modify input layer (torchvision ResNet expects 3 input channels)
+    model._modules['backbone']['conv1'] = torch.nn.Conv2d(
+                                              in_channels=n_channels, out_channels=64,
+                                              kernel_size=(k,k), stride=1, padding=(k+1)//2
+                                              )
+    # modify output layer (torchvision ResNet predicts 21 output channels)
+    model._modules['classifier'][-1] = torch.nn.Conv2d(
+                                              in_channels=512, out_channels=2,
+                                              kernel_size=(k,k), stride=1, padding=(k+1)//2
+                                              )    
+
+    def model_forward(input):
+        return model.forward(input)['out'] # because reasons...
+        
+else: 
+    raise NotImplementedError()
 
 print('total #parameters: ', np.sum([np.prod(item.shape) for item in model.state_dict().values()]))
+print('output shape: ', model_forward(torch.zeros((7,n_channels,32,64))).shape)
 
 
 ## train model
@@ -127,24 +180,27 @@ import torch.optim as optim
 import torch.nn.functional as F
 from copy import deepcopy
 
-optimizer = optim.Adam(model.parameters(), lr=1e-4)
+optimizer = optim.Adam(model.parameters(), lr=0.001)
 
 n_epochs, max_patience = 200, 20
-losses, best_loss, patience = np.zeros(n_epochs), np.inf, max_patience
+best_loss, patience = np.inf, max_patience
 best_state_dict = {}
 
 epoch = 0
+model.train()
 while True:
 
     epoch += 1
     if epoch > n_epochs:
         break
 
+    print(f'epoch #{epoch}')
     # Train for a single epoch.
     for batch in train_loader:
         optimizer.zero_grad()
         inputs, targets = batch[0].to(device), batch[1].to(device)
-        loss = F.mse_loss(model.forward(inputs), targets)
+        loss = F.mse_loss(model_forward(inputs), targets)
+        print('computed minibatch loss')
         loss.backward()
         optimizer.step()
 
@@ -154,7 +210,7 @@ while True:
         nb = 0
         for batch in validation_loader:
             inputs, targets = batch[0].to(device), batch[1].to(device)
-            val_loss += F.mse_loss(model.forward(inputs), targets)
+            val_loss += F.mse_loss(model_forward(inputs), targets)
             nb += 1
     val_loss /= nb
     print(f'epoch #{epoch} || loss (last batch) {loss} || validation loss {val_loss}')
@@ -173,4 +229,3 @@ while True:
         break
 
 torch.save(best_state_dict, res_dir + model_fn) # create savefile in case we never beat initial loss...
-        
