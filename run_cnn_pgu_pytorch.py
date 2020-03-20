bab58b638d5819ffffae96959c9134da52d04496
mnonnenm <mnonnenm@uos.de>
2020-03-20 21:48:55 +0100

    increasing number of checkpoints during training and adding evaluation to script

--- run_cnn_pgu_pytorch.py
+++ run_cnn_pgu_pytorch.py
@@ -187,6 +187,7 @@ best_loss, patience = np.inf, max_patience
 best_state_dict = {}
 
 epoch = 0
+num_steps, eval_every = 0, 10000
 model.train()
 while True:
 
@@ -202,29 +203,65 @@ while True:
         loss = F.mse_loss(model_forward(inputs), targets)
         loss.backward()
         optimizer.step()
+        num_steps += 1
 
-    # Track convergence on validation set.
-    val_loss = 0
-    with torch.no_grad():
-        nb = 0
-        for batch in validation_loader:
-            inputs, targets = batch[0].to(device), batch[1].to(device)
-            val_loss += F.mse_loss(model_forward(inputs), targets)
-            nb += 1
-    val_loss /= nb
-    print(f'epoch #{epoch} || loss (last batch) {loss} || validation loss {val_loss}')
+        # Track convergence on validation set.
+        if np.mod(num_setps, eval_every) == 0:
+            val_loss = 0
+            with torch.no_grad():
+                nb = 0
+                for batch in validation_loader:
+                    inputs, targets = batch[0].to(device), batch[1].to(device)
+                    val_loss += F.mse_loss(model_forward(inputs), targets)
+                    nb += 1
+            val_loss /= nb
+            print(f'epoch #{epoch} || loss (last batch) {loss} || validation loss {val_loss}')
 
-    if val_loss < best_loss:
-        patience = max_patience
-        best_loss = val_loss
-        best_state_dict = deepcopy(model.state_dict()) # during early training will save every epoch
-        torch.save(best_state_dict, res_dir + model_fn)
+        if val_loss < best_loss:
+            patience = max_patience
+            best_loss = val_loss
+            best_state_dict = deepcopy(model.state_dict()) # during early training will save every epoch
+            torch.save(best_state_dict, res_dir + model_fn)
 
-    else:
-        patience -= 1
+        else:
+            patience -= 1
 
     if patience == 0:
         model.load_state_dict(best_state_dict)
         break
 
 torch.save(best_state_dict, res_dir + model_fn) # create savefile in case we never beat initial loss...
+
+# model evaluation
+dg_test =  Dataset(x.sel(time=slice('2017', '2018')),
+                   var_dict,
+                   lead_time,
+                   mean=dg_train.mean, # make sure that model was trained 
+                   std=dg_train.std,   # with same data as in dg_train, 
+                   normalize=True,     # or else normalization is off!
+                   randomize_order=False)
+
+test_loader = torch.utils.data.DataLoader(
+    dg_test, batch_size=batch_size, drop_last=False
+)
+
+model.eval() # e.g. for batch normalization layerspreds = []
+preds = []
+with torch.no_grad():
+    for i,batch in enumerate(test_loader):
+        #print('batch #' + str(i))
+        inputs = batch[0]
+        pred = model_forward(inputs)
+        preds += [pred]
+idx = dg_test._target_idx
+preds = np.concatenate(preds, axis=0) * dg_test.std.values[None,idx,None,None] + dg_test.mean.values[None,idx,None,None]
+
+from src.score import compute_weighted_rmse, load_test_data
+z500_test = load_test_data(f'{datadir}geopotential_500/', 'z')
+t850_test = load_test_data(f'{datadir}temperature_850/', 't')
+rmse_z = compute_weighted_rmse(preds[:,0,:,:], z500_test.isel(time=slice(lead_time, None))).load()
+rmse_t = compute_weighted_rmse(preds[:,1,:,:], t850_test.isel(time=slice(lead_time, None))).load()
+print('RMSE z', rmse_z.values); print('RMSE t', rmse_t.values)
+
+print('saving RMSE results to ' + res_dir + model_fn[:-3] + '_RMSE_zt.npy')
+np.save(res_dir + model_fn[:-3] + '_RMSE_zt',  np.hstack((rmse_z.values, rmse_t.values)))
