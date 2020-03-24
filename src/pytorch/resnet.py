84987072ca0a613924d4fad6af3c9bfebb64a2c7
mnonnenm <mnonnenm@uos.de>
2020-03-24 20:06:06 +0100

    updating ResNet implementation with dropout weights - essentially killing backwards-compatibility with torchvision resnets. - still 1x1 convolution in final layer

--- src/pytorch/resnet.py
+++ src/pytorch/resnet.py
@@ -1,21 +1,22 @@
 import torch
 from torchvision.models.resnet import ResNet
-from torchvision.models.resnet import Bottleneck
+from torchvision.models.resnet import Bottleneck, conv1x1
 from .layers import PeriodicConv2D
 
 
 
-def circConv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1, kernel_size=3, bias=False, padding_mode='circular'):
+def circConv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1, kernel_size=3, padding_mode='circular'):
     """3x3 convolution with padding"""
     return PeriodicConv2D(in_planes, out_planes, kernel_size=kernel_size, stride=stride,
-                     padding=kernel_size-1, groups=groups, bias=bias, dilation=dilation,
+                     padding=kernel_size-1, groups=groups, bias=True, dilation=dilation,
                      padding_mode=padding_mode)
 
 class CircBlock(torch.nn.Module):
     expansion = 1
 
-    def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1, kernel_size=3,
-                 base_width=64, dilation=1, norm_layer=None, padding_mode='circular'):
+    def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1, 
+                 base_width=64, dilation=1, norm_layer=None, 
+                 kernel_size=3, padding_mode='circular', dropout_rate=0):
         super(CircBlock, self).__init__()
         if norm_layer is None:
             norm_layer = torch.nn.BatchNorm2d
@@ -27,6 +28,7 @@ class CircBlock(torch.nn.Module):
         self.conv1 = circConv3x3(inplanes, planes, stride, kernel_size=kernel_size, padding_mode=padding_mode)
         self.bn1 = norm_layer(planes)
         self.relu = torch.nn.ReLU(inplace=True)
+        self.dropout = torch.nn.Dropout(p=dropout_rate) if dropout_rate > 0 else torch.nn.Identity()
         self.conv2 = circConv3x3(planes, planes, kernel_size=kernel_size, padding_mode=padding_mode)
         self.bn2 = norm_layer(planes)
         self.downsample = downsample
@@ -38,15 +40,17 @@ class CircBlock(torch.nn.Module):
         out = self.conv1(x)
         out = self.bn1(out)
         out = self.relu(out)
-
+        out = self.dropout(out)
         out = self.conv2(out)
         out = self.bn2(out)
 
         if self.downsample is not None:
             identity = self.downsample(x)
 
-        out += identity
         out = self.relu(out)
+        out = self.dropout(out) # 2nd application of Dropout with own call to torch.functional.Dropout
+            
+        out += identity
 
         return out
 
@@ -57,22 +61,15 @@ class FCNResNet(ResNet):
     https://github.com/pytorch/vision/blob/master/torchvision/models/resnet.py#L124
     """
     def __init__(self, block, layers, zero_init_residual=False,
-                 groups=1, width_per_group=64, replace_stride_with_dilation=None,
+                 groups=1, width_per_group=64,
                  norm_layer=None, in_channels=1, out_channels=1, 
-                 nfilters=[64, 64, 128, 256, 512], kernel_size=3, padding_mode='zeros'):
+                 nfilters=[64, 64, 128, 256, 512], kernel_size=3, padding_mode='zeros',
+                 dropout_rate=0):
         super(ResNet, self).__init__()
+
         if norm_layer is None:
             norm_layer = torch.nn.BatchNorm2d
         self._norm_layer = norm_layer
-
-        self.dilation = 1
-        if replace_stride_with_dilation is None:
-            # each element in the tuple indicates if we should replace
-            # the 2x2 stride with a dilated convolution instead
-            replace_stride_with_dilation = [False, False, False]
-        if len(replace_stride_with_dilation) != 3:
-            raise ValueError("replace_stride_with_dilation should be None "
-                             "or a 3-element tuple, got {}".format(replace_stride_with_dilation))
         self.groups = groups
         self.base_width = width_per_group
 
@@ -82,31 +79,25 @@ class FCNResNet(ResNet):
         self.conv1 = PeriodicConv2D(in_channels, nfilters[0], kernel_size=kernel_size, 
                                     stride=1,  padding=kernel_size-1, bias=True,
                                     padding_mode=padding_mode)
-
-        
-        
         self.bn1 = norm_layer(nfilters[0])
         self.relu = torch.nn.ReLU(inplace=True)
+        self.dropout = torch.nn.Dropout(p=dropout_rate) if dropout_rate > 0 else torch.nn.Identity()
 
-        self.layer1 = self._make_layer(block, nfilters[1], layers[0])
+        self.layer1 = self._make_layer(block, nfilters[1], layers[0], dr=dropout_rate)
         if len(layers) > 2:
-            self.layer2 = self._make_layer(block, nfilters[2], layers[1], stride=2,
-                                           dilate=replace_stride_with_dilation[0])
+            self.layer2 = self._make_layer(block, nfilters[2], layers[1], dr=dropout_rate)
         if len(layers) > 3:
-            self.layer3 = self._make_layer(block, nfilters[3], layers[2], stride=2,
-                                           dilate=replace_stride_with_dilation[1])
+            self.layer3 = self._make_layer(block, nfilters[3], layers[2], dr=dropout_rate)
         if len(layers) > 4:
-            self.layer4 = self._make_layer(block, nfilters[4], layers[3], stride=2,
-                                           dilate=replace_stride_with_dilation[2])
+            self.layer4 = self._make_layer(block, nfilters[4], layers[3], dr=dropout_rate)
 
         expansion = 4 if block is Bottleneck else 1
         self.final = torch.nn.Conv2d(in_channels=expansion*nfilters[len(layers)],
                                      out_channels=out_channels, 
                                      kernel_size=(1,1), 
-                                     stride=1) 
-
+                                     stride=1)
         for m in self.modules():
-            if isinstance(m, torch.nn.Conv2d):
+            if isinstance(m, torch.nn.Conv2d): # also covers CircularConv2D
                 torch.nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
             elif isinstance(m, (torch.nn.BatchNorm2d, torch.nn.GroupNorm)):
                 torch.nn.init.constant_(m.weight, 1)
@@ -120,7 +111,28 @@ class FCNResNet(ResNet):
                 if isinstance(m, Bottleneck):
                     torch.nn.init.constant_(m.bn3.weight, 0)
                 elif isinstance(m, BasicBlock):
-                    torch.nn.init.constant_(m.bn2.weight, 0)    
+                    torch.nn.init.constant_(m.bn2.weight, 0)
+
+
+    def _make_layer(self, block, planes, blocks, stride=1, dilation=1, dr=0):
+        norm_layer = self._norm_layer
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = torch.nn.Sequential(
+                conv1x1(self.inplanes, planes * block.expansion, stride),
+                norm_layer(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample, self.groups,
+                            self.base_width, dilation, norm_layer, dropout_rate=dr))
+        self.inplanes = planes * block.expansion
+        for _ in range(1, blocks):
+            layers.append(block(self.inplanes, planes, groups=self.groups,
+                                base_width=self.base_width, dilation=dilation,
+                                norm_layer=norm_layer, dropout_rate=dr))
+
+        return torch.nn.Sequential(*layers)
 
     def _forward_impl(self, x):
         # See note [TorchScript super()]
@@ -129,6 +141,7 @@ class FCNResNet(ResNet):
         x = self.conv1(x)
         x = self.bn1(x)
         x = self.relu(x)
+        x = self.dropout(x)
 
         # actual ResNet layers (multiple ResNet blocks per layer)
         x = self.layer1(x)
