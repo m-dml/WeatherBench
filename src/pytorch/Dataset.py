ab673aa3691f2c9ede27cab75d83be2a198107b9
mnonnenm <mnonnenm@uos.de>
2020-03-30 19:44:53 +0200

    bunch of fixes around __iter__() method and new feature for multiple time points

--- src/pytorch/Dataset.py
+++ src/pytorch/Dataset.py
@@ -51,12 +51,12 @@ class Dataset(torch.utils.data.IterableDataset):
         assert np.all(var in var_dict.keys() for var in target_var_dict.keys())
         assert np.all(level in var_dict[var][1] for var, level in target_var_dict.items())
         
-        max_input_lag = -np.min(self.past_times) if len(self.past_times) > 0 else 0
+        self.max_input_lag = -np.min(self.past_times) if len(self.past_times) > 0 else 0
         if start is None or end is None:
-            start = np.max([0, max_input_lag])
+            start = np.max([0, self.max_input_lag])
             end = self.ds.time.isel(time=slice(0, -self.lead_time)).values.shape[0]
         assert end > start, "this example code only works with end >= start"
-        assert start >= max_input_lag
+        assert start >= self.max_input_lag
         self.start, self.end = start, end
 
         self.data = []
@@ -116,11 +116,11 @@ class Dataset(torch.utils.data.IterableDataset):
         assert np.min(index) >= self.start
         idx = np.asarray(index)
         X = self.data.isel(time=idx).values
-        if max_input_lag > 0:
-            Xl = []
+        if self.max_input_lag > 0:
+            Xl = [X]
             for l in self.past_times:
-                Xl.append(self.data.isel(time=idx+self.past_times[l]).values)
-            X = np.concatenate(Xls, axis=1) # stack past time points along channel dimension            
+                Xl.append(self.data.isel(time=idx+l).values)
+            X = np.concatenate(Xl, axis=1) if len (idx) > 1 else np.concatenate(Xl, axis=0)         
         y = self.data.isel(time=idx + self.lead_time, level=self._target_idx).values
         return X, y
 
@@ -130,25 +130,26 @@ class Dataset(torch.utils.data.IterableDataset):
         """ Return iterable over data in random order """
         worker_info = torch.utils.data.get_worker_info()
         if worker_info is None:  # single-process data loading, return the full iterator
-            iter_start = self.start
-            iter_end = self.end
+            iter_start = torch.tensor(self.start, requires_grad=False, dtype=torch.int)
+            iter_end = torch.tensor(self.end, requires_grad=False, dtype=torch.int)
         else:  # in a worker process
             # split workload
             per_worker = int(math.ceil((self.end - self.start) / float(worker_info.num_workers)))
             worker_id = worker_info.id
-            iter_start = self.start + worker_id * per_worker
-            iter_end = min(iter_start + per_worker, self.end)
+            iter_start = torch.tensor(self.start + worker_id * per_worker, requires_grad=False, dtype=torch.int)
+            iter_end = torch.tensor(min(iter_start + per_worker, self.end), requires_grad=False, dtype=torch.int)
 
         if self.randomize_order:
-            idx = torch.randperm(iter_end - iter_start).cpu() + iter_start  # torch for seed control
+            idx = torch.randperm((iter_end - iter_start) + iter_start).cpu()
         else: 
-            idx = torch.arange(iter_start, iter_end).cpu()
+            idx = torch.arange(iter_start, iter_end, requires_grad=False).cpu()
+
         X = self.data.isel(time=idx).values
-        if max_input_lag > 0:
-            Xl = []
+        if self.max_input_lag > 0:
+            Xl = [X]
             for l in self.past_times:
-                Xl.append(self.data.isel(time=idx+self.past_times[l]).values)
-            X = np.concatenate(Xls, axis=1) # stack past time points along channel dimension        
+                Xl.append(self.data.isel(time=idx+l).values)
+            X = np.concatenate(Xl, axis=1) # stack past time points along channel dimension        
         y = self.data.isel(time=idx + self.lead_time, level=self._target_idx).values
         return zip(X, y)
     
