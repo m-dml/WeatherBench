4440a3c378b53958a88716474980f4f8cb0c525e
mnonnenm <mnonnenm@uos.de>
2020-04-09 04:39:51 +0200

    updating dev notebook and GPU node test script

--- test_streaming_loader_pgpu.py
+++ test_streaming_loader_pgpu.py
@@ -25,7 +25,7 @@ model_name = 'simpleResnet' # 'simpleResnet', 'tvfcnResnet50', 'cnnbn', 'Unetbn'
 lead_time = 3*24
 batch_size = 32
 
-train_years = ('2015', '2015')
+train_years = ('1979', '2015')
 validation_years = ('2016', '2016')
 test_years = ('2017', '2018')
 
@@ -36,7 +36,7 @@ var_dict = {'geopotential': ('z', [100, 200, 500, 850, 1000]),
            'constants': ['lsm','orography','lat2d']
            }
 
-target_var_dict = {'geopotential': 100, 'temperature': 850}
+target_var_dict = {'geopotential': 500, 'temperature': 850}
 
 filters = [128, 128, 128, 128, 128, 128, 128, 128, 128, 128, 128, 128, 128, 128]
 kernel_sizes = [7, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
@@ -47,7 +47,7 @@ loss_fun = 'mse'
 
 ################################################################################
 
-from src.pytorch.Dataset import Dataset_memmap
+from src.pytorch.Dataset import Dataset_memmap, collate_fn_memmap
 
 filedir = datadir + '5_625deg_all_zscored.npy'
 leveldir = datadir + '5_625deg_all_level_names.npy'
@@ -58,8 +58,12 @@ dg_train = Dataset_memmap(filedir=filedir, leveldir=leveldir,
                           target_var_dict=target_var_dict, 
                           dtype=np.float32, past_times=past_times, verbose=verbose)
 
+def collate_fn(batch):
+    return collate_fn_memmap(batch, dg_train)
+
 train_loader = torch.utils.data.DataLoader(
     dg_train,
+    collate_fn=collate_fn,
     batch_size=batch_size,
     num_workers=0,
     drop_last=True)  
@@ -76,11 +80,10 @@ def do_dummy_epoch(train_loader, t = None):
     num_steps = 1
     t = time.time() if t is None else t
     for batch in train_loader:
+        inputs, targets = batch[0].to(device), batch[1].to(device)
         if np.mod(num_steps, print_every) == 0 or num_steps == 1:
             print(f"- batch #{num_steps}, time: {'{0:.2f}'.format(time.time() - t)}")
-        inputs, targets = batch[0].to(device), batch[1].to(device)
-        out = (inputs.shape, targets.shape)
-        #print(out)
+            #print(inputs.shape, targets.shape)
         num_steps +=1
         if num_steps > max_steps:
             break
