fixing seemingly critical issue with parameter initialization in MHSA layer - turns out initializing at all-zero weight matrix also sets gradient to zero?

src/pytorch/transformers.py
@@ -114,12 +114,9 @@ class ConvMHSA(torch.nn.Module):
                               bias=self.bias, 
                               padding_mode=padding_mode)
 
-        self.W_out = torch.nn.Parameter(
-            torch.zeros(self.N_h * self.D_h, self.D_out)
-        )
-        self.b_out = torch.nn.Parameter(
-            torch.zeros(1,1,self.D_out,1,1)
-        )
+        self.W_out = torch.nn.Parameter(torch.zeros(self.N_h * self.D_h, self.D_out))
+        torch.nn.init.kaiming_uniform_(self.W_out, a=0, mode='fan_in', nonlinearity='linear')
+        self.b_out = torch.nn.Parameter(torch.zeros(1,1,self.D_out,1,1))
 
     def forward(self, x):
 