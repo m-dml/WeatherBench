e38dd6b666d9882b2bf2b4c703c8c965e11f4fdc
mnonnenm <mnonnenm@uos.de>
2020-06-04 18:02:50 +0200

    extending test script to track reasons why the full architecture doesn't train - all weights are updated during SGD, so computational graph seems intact?

--- quick_test_convtransformer.py
+++ quick_test_convtransformer.py
@@ -2,12 +2,15 @@ import torch
 import numpy as np
 from src.pytorch.util import init_torch_device
 from src.pytorch.util import named_network
+import copy
+import torch.optim as optim
 
 device = init_torch_device()
 
-n_channels = 23
+n_channels = 24
 n_output = 2
 seq_length = 13
+
 
 kwargs = {
     # initial convolutional layers applied to each sequence member separately
@@ -17,7 +20,8 @@ kwargs = {
     # Transformer layers with feedforward networks being convolutional residual nets
     'kernel_sizes' : [3, 3, 3],
     'filters' : [32, 32, 64],                     # n_channels for output stage of MHSA
-    'filters_ff' : [[64,64], [64,64], [128,128]], # n_filters for feedforward blocks
+    'filters_ff' : [64, 64, 128],                 # n_filters for feedforward blocks
+    'nresblocks_ff' : [1, 1, 1],
     'N_h' : 8,
     'D_h' : 8,
     'D_k' : 16,
@@ -38,13 +42,41 @@ model, model_forward, _ = named_network(model_name, n_channels, n_output, seq_le
 
 
 inputs = torch.tensor(
-    np.random.normal(size=(16, seq_length, 23, 32, 64)),
+    np.random.normal(size=(16, seq_length, n_channels, 32, 64)),
     requires_grad=False,
     dtype=torch.float32)
 print(model_forward(inputs).shape)
 
+
+#print('example model weights, pre', model.state_dict()['layers.0.sattn.b_out'])
+
+
+
 print('total #parameters: ', np.sum([np.prod(item.shape) for item in model.state_dict().values()]))
 
-loss = torch.sum(model_forward(inputs) - inputs[:,0,:2,:,:])
-loss.backward()
-print('took gradient')
+old_state_dict =  model.state_dict().copy()
+for key, value in old_state_dict.items():
+    old_state_dict[key] = copy.deepcopy(value)
+
+    
+lr, weight_decay = 5e-4, 0. 
+optimizer = optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
+for i in range(5):
+    loss = torch.sum((model.forward(inputs) - inputs[:,0,:2,:,:])**2)
+    loss.backward()
+    optimizer.step()
+    print('took gradient')
+
+
+#print('example model weights, post', model.state_dict()['layers.0.sattn.b_out'])
+    
+new_state_dict =  model.state_dict()
+
+diff_list = [np.max(np.abs(x.detach().cpu().numpy()-y.detach().cpu().numpy())) for x,y in zip(old_state_dict.values(), new_state_dict.values())]
+
+min_diff = np.min( diff_list )
+max_diff = np.max( diff_list ) 
+
+print(max_diff, min_diff)
+for x,y in zip(old_state_dict.keys(), diff_list):
+    print(f'{x}, {y}, \n') 
